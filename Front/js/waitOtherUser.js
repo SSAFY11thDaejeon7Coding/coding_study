dbafe5f237b309e7eb5adad66674f4fd12f5f140
Yebin <ferf99@naver.com>
2024-03-11 23:38:19 +0900

    폰트 크기 조건 수정

--- Front/js/waitOtherUser.js
+++ Front/js/waitOtherUser.js
@@ -1,9 +1,9 @@
 const userNames = document.querySelectorAll('.userName');
 userNames.forEach(userName => {
     const nameLength = userName.innerText.length;
-    if (nameLength <= 5) {
+    if (nameLength <= 4) {
         userName.classList.add('small');
-    } else if (nameLength > 5 && nameLength <= 10) {
+    } else if (nameLength > 4 && nameLength <= 7) {
         userName.classList.add('medium');
     } else {
         userName.classList.add('large');
